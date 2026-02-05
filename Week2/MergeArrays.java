140c3f3d729c8dc33ecc77b35ad62ae02941cd9c
Nishant sharma <sharmanishant9818@gmail.com>
2026-02-05 23:31:21 +0530

    week2 – problem2 – Merge Arrays
    Implement mergeArrays method to print elements from two arrays.

---
+++ Week2/MergeArrays.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void mergeArrays(int[] a,int[] b){
+
+        for(int i=0;i<a.length;i++)
+            System.out.print(a[i]+" ");
+
+        for(int i=0;i<b.length;i++)
+            System.out.print(b[i]+" ");
+    }
+}
